4b57e5110c71466b017e00046d3ddf95fefea1a7
DinhThuong <dinhthuong092@gmail.com>
2021-03-31 19:40:56 +0700

    lan 31

--- pages/index.js
+++ pages/index.js
@@ -68,8 +68,7 @@ export default function Home({ kq }) {
 
   useEffect(() => {
     const flag = localStorage.getItem("flagData");
-    flag && 
-    getEmail();
+    flag && getEmail();
     getNewFeed();
     fetchData();
   }, []);
